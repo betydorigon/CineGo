d5f8b2f3b6920a215215b08d8861a73300d9ee83
Jessica <jessicagavazzi2006@gmail.com>
2026-08-07 17:25:23 -0300

    Fazendo Form de Login se quiser pode fazer tmb :]

---
+++ CineGo.Desktop/Forms/LoginForm.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace CineGo.Desktop.Forms
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges11 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges12 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges13 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges14 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges15 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges16 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges17 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges18 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges19 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges20 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            lblTituloBemVindo = new Label();
+            lblTextoFacaLogin = new Label();
+            lblEmail = new Label();
+            lblSenha = new Label();
+            lblConfirmarSenha = new Label();
+            txtEmail = new Guna.UI2.WinForms.Guna2TextBox();
+            txtSenha = new Guna.UI2.WinForms.Guna2TextBox();
+            txtConfirmarSenha = new Guna.UI2.WinForms.Guna2TextBox();
+            btnEntrar = new Guna.UI2.WinForms.Guna2Button();
+            pbLogo = new Guna.UI2.WinForms.Guna2PictureBox();
+            pnlSeparador = new Panel();
+            pnlSeparador2 = new Panel();
+            lblProblemasEmAcessar = new Label();
+            lblAPI = new Label();
+            lblErro = new Label();
+            ((System.ComponentModel.ISupportInitialize)pbLogo).BeginInit();
+            SuspendLayout();
+            // 
+            // lblTituloBemVindo
+            // 
+            lblTituloBemVindo.AutoSize = true;
+            lblTituloBemVindo.Location = new Point(181, 34);
+            lblTituloBemVindo.Name = "lblTituloBemVindo";
+            lblTituloBemVindo.Size = new Size(69, 15);
+            lblTituloBemVindo.TabIndex = 1;
+            lblTituloBemVindo.Text = "Bem-vindo!";
+            // 
+            // lblTextoFacaLogin
+            // 
+            lblTextoFacaLogin.AutoSize = true;
+            lblTextoFacaLogin.Location = new Point(181, 70);
+            lblTextoFacaLogin.Name = "lblTextoFacaLogin";
+            lblTextoFacaLogin.Size = new Size(151, 15);
+            lblTextoFacaLogin.TabIndex = 2;
+            lblTextoFacaLogin.Text = "Faça login com a sua conta";
+            // 
+            // lblEmail
+            // 
+            lblEmail.AutoSize = true;
+            lblEmail.Location = new Point(19, 136);
+            lblEmail.Name = "lblEmail";
+            lblEmail.Size = new Size(36, 15);
+            lblEmail.TabIndex = 3;
+            lblEmail.Text = "Email";
+            // 
+            // lblSenha
+            // 
+            lblSenha.AutoSize = true;
+            lblSenha.Location = new Point(19, 201);
+            lblSenha.Name = "lblSenha";
+            lblSenha.Size = new Size(39, 15);
+            lblSenha.TabIndex = 4;
+            lblSenha.Text = "Senha";
+            // 
+            // lblConfirmarSenha
+            // 
+            lblConfirmarSenha.AutoSize = true;
+            lblConfirmarSenha.Location = new Point(19, 265);
+            lblConfirmarSenha.Name = "lblConfirmarSenha";
+            lblConfirmarSenha.Size = new Size(95, 15);
+            lblConfirmarSenha.TabIndex = 5;
+            lblConfirmarSenha.Text = "Confirmar senha";
+            // 
+            // txtEmail
+            // 
+            txtEmail.CustomizableEdges = customizableEdges11;
+            txtEmail.DefaultText = "";
+            txtEmail.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            txtEmail.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            txtEmail.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            txtEmail.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            txtEmail.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            txtEmail.Font = new Font("Segoe UI", 9F);
+            txtEmail.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            txtEmail.Location = new Point(19, 157);
+            txtEmail.Name = "txtEmail";
+            txtEmail.PlaceholderText = "";
+            txtEmail.SelectedText = "";
+            txtEmail.ShadowDecoration.CustomizableEdges = customizableEdges12;
+            txtEmail.Size = new Size(200, 36);
+            txtEmail.TabIndex = 6;
+            // 
+            // txtSenha
+            // 
+            txtSenha.CustomizableEdges = customizableEdges13;
+            txtSenha.DefaultText = "";
+            txtSenha.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            txtSenha.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            txtSenha.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            txtSenha.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            txtSenha.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            txtSenha.Font = new Font("Segoe UI", 9F);
+            txtSenha.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            txtSenha.Location = new Point(19, 222);
+            txtSenha.Name = "txtSenha";
+            txtSenha.PlaceholderText = "";
+            txtSenha.SelectedText = "";
+            txtSenha.ShadowDecoration.CustomizableEdges = customizableEdges14;
+            txtSenha.Size = new Size(200, 36);
+            txtSenha.TabIndex = 7;
+            // 
+            // txtConfirmarSenha
+            // 
+            txtConfirmarSenha.CustomizableEdges = customizableEdges15;
+            txtConfirmarSenha.DefaultText = "";
+            txtConfirmarSenha.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            txtConfirmarSenha.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            txtConfirmarSenha.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            txtConfirmarSenha.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            txtConfirmarSenha.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            txtConfirmarSenha.Font = new Font("Segoe UI", 9F);
+            txtConfirmarSenha.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            txtConfirmarSenha.Location = new Point(19, 285);
+            txtConfirmarSenha.Name = "txtConfirmarSenha";
+            txtConfirmarSenha.PlaceholderText = "";
+            txtConfirmarSenha.SelectedText = "";
+            txtConfirmarSenha.ShadowDecoration.CustomizableEdges = customizableEdges16;
+            txtConfirmarSenha.Size = new Size(200, 36);
+            txtConfirmarSenha.TabIndex = 8;
+            // 
+            // btnEntrar
+            // 
+            btnEntrar.CustomizableEdges = customizableEdges17;
+            btnEntrar.DisabledState.BorderColor = Color.DarkGray;
+            btnEntrar.DisabledState.CustomBorderColor = Color.DarkGray;
+            btnEntrar.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            btnEntrar.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            btnEntrar.Font = new Font("Segoe UI", 9F);
+            btnEntrar.ForeColor = Color.White;
+            btnEntrar.Location = new Point(90, 337);
+            btnEntrar.Name = "btnEntrar";
+            btnEntrar.ShadowDecoration.CustomizableEdges = customizableEdges18;
+            btnEntrar.Size = new Size(229, 45);
+            btnEntrar.TabIndex = 9;
+            btnEntrar.Text = "Entrar";
+            // 
+            // pbLogo
+            // 
+            pbLogo.CustomizableEdges = customizableEdges19;
+            pbLogo.ImageRotate = 0F;
+            pbLogo.Location = new Point(19, 12);
+            pbLogo.Name = "pbLogo";
+            pbLogo.ShadowDecoration.CustomizableEdges = customizableEdges20;
+            pbLogo.Size = new Size(141, 90);
+            pbLogo.TabIndex = 10;
+            pbLogo.TabStop = false;
+            // 
+            // pnlSeparador
+            // 
+            pnlSeparador.BackColor = SystemColors.ActiveCaptionText;
+            pnlSeparador.Location = new Point(19, 121);
+            pnlSeparador.Name = "pnlSeparador";
+            pnlSeparador.Size = new Size(371, 1);
+            pnlSeparador.TabIndex = 11;
+            // 
+            // pnlSeparador2
+            // 
+            pnlSeparador2.BackColor = SystemColors.ActiveCaptionText;
+            pnlSeparador2.Location = new Point(19, 399);
+            pnlSeparador2.Name = "pnlSeparador2";
+            pnlSeparador2.Size = new Size(371, 1);
+            pnlSeparador2.TabIndex = 11;
+            // 
+            // lblProblemasEmAcessar
+            // 
+            lblProblemasEmAcessar.AutoSize = true;
+            lblProblemasEmAcessar.Location = new Point(19, 407);
+            lblProblemasEmAcessar.Name = "lblProblemasEmAcessar";
+            lblProblemasEmAcessar.Size = new Size(329, 15);
+            lblProblemasEmAcessar.TabIndex = 5;
+            lblProblemasEmAcessar.Text = "Problemas para acessar? Contate o administrador do servidor";
+            // 
+            // lblAPI
+            // 
+            lblAPI.AutoSize = true;
+            lblAPI.Location = new Point(19, 429);
+            lblAPI.Name = "lblAPI";
+            lblAPI.Size = new Size(40, 15);
+            lblAPI.TabIndex = 5;
+            lblAPI.Text = "API: ...";
+            // 
+            // lblErro
+            // 
+            lblErro.AutoSize = true;
+            lblErro.Location = new Point(19, 450);
+            lblErro.Name = "lblErro";
+            lblErro.Size = new Size(43, 15);
+            lblErro.TabIndex = 5;
+            lblErro.Text = "Erro: ...";
+            // 
+            // LoginForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(406, 481);
+            Controls.Add(pnlSeparador2);
+            Controls.Add(pnlSeparador);
+            Controls.Add(pbLogo);
+            Controls.Add(btnEntrar);
+            Controls.Add(txtConfirmarSenha);
+            Controls.Add(txtSenha);
+            Controls.Add(txtEmail);
+            Controls.Add(lblErro);
+            Controls.Add(lblAPI);
+            Controls.Add(lblProblemasEmAcessar);
+            Controls.Add(lblConfirmarSenha);
+            Controls.Add(lblSenha);
+            Controls.Add(lblEmail);
+            Controls.Add(lblTextoFacaLogin);
+            Controls.Add(lblTituloBemVindo);
+            Name = "LoginForm";
+            Text = "LoginForm";
+            ((System.ComponentModel.ISupportInitialize)pbLogo).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private Label lblTituloBemVindo;
+        private Label lblTextoFacaLogin;
+        private Label lblEmail;
+        private Label lblSenha;
+        private Label lblConfirmarSenha;
+        private Guna.UI2.WinForms.Guna2TextBox txtEmail;
+        private Guna.UI2.WinForms.Guna2TextBox txtSenha;
+        private Guna.UI2.WinForms.Guna2TextBox txtConfirmarSenha;
+        private Guna.UI2.WinForms.Guna2Button btnEntrar;
+        private Guna.UI2.WinForms.Guna2PictureBox pbLogo;
+        private Panel pnlSeparador;
+        private Panel pnlSeparador2;
+        private Label lblProblemasEmAcessar;
+        private Label lblAPI;
+        private Label lblErro;
+    }
+}
