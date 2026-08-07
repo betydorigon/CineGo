e9f12b3fdae7b80f103b09ea7b5d5b2c4e4c978e
LUCAS <lucarty79@gmail.com>
2026-08-07 17:24:59 -0300

    eu 1724

---
+++ CineGo.Desktop/DTOs/UsuarioDtos.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CineGo.Desktop.DTOs
+{
+    public class UsuarioResponseDtos
+    {
+        public string Id { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Perfil { get; set; } = string.Empty;
+
+    }
+
+    public class CreateUsuarioDto
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string ConfirmPassword { get; set; } = string.Empty;
+        public string Perfil { get; set; } = string.Empty;
+    }
+
+    public class UpdateUsuarioDto
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string ConfirmPassword { get; set; } = string.Empty;
+        public string Perfil { get; set; } = string.Empty;
+    }
+}
